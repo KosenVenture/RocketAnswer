8ae1adab47b519b900f120d1105d67daffbeac4a
Hayato OKUMOTO <falcon@falconsrv.net>
2014-04-04 01:44:46 +0900

    alertの調整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,17 @@
 module ApplicationHelper
   def bootstrap_alert
     content_tag :div, class: 'alert alert-danger' do
-      alert
+      concat content_tag(:button, '×', class: 'close', data: { dismiss: 'alert' })
+      concat content_tag(:i, '', class: 'fa fa-exclamation-triangle')
+      concat content_tag(:span, alert, class: 'message')
     end if alert
   end
 
   def bootstrap_notice
     content_tag :div, class: 'alert alert-info' do
-      notice
+      concat content_tag(:button, '×', class: 'close', data: { dismiss: 'alert' })
+      concat content_tag(:i, '', class: 'fa fa-info-circle')
+      concat content_tag(:span, notice, class: 'message')
     end if notice
   end
 end
