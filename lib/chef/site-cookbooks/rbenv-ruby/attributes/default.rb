8eaa20ea06e5cde8aaea153fc0a1fb80471581de
Hayato OKUMOTO <falcon@falconsrv.net>
2014-04-07 00:58:37 +0900

    Rails環境設定用のChefスクリプトを追加(#56)

---
+++ lib/chef/site-cookbooks/rbenv-ruby/attributes/default.rb
@@ -0,0 +1,2 @@
+default['rbenv-ruby']['ruby-version'] = '2.0.0-p0'
+default['rbenv-ruby']['gems'] = []
---
+++ lib/chef/site-cookbooks/rbenv-ruby/metadata.rb
@@ -0,0 +1,9 @@
+name             'rbenv-ruby'
+maintainer       'Hayato OKUMOTO(falcon8823)'
+maintainer_email 'falcon@falconsrv.net'
+license          'All rights reserved'
+description      'Installs/Configures Ruby with using rbenv'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+recipe 'rbenv-ruby', 'Installs Ruby with using rbenv'
