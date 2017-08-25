dcd0f9faf48644c59e9570c4fb1af84a58e406fd
Snow-Poijio <snow.poijio@gmail.com>
2017-08-26 00:31:30 +0900

    Add form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170820134158) do
+ActiveRecord::Schema.define(version: 20170825143344) do
 
   create_table "active_admin_comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "namespace"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20170820134158) do
     t.integer "department_id"
     t.string "subject"
     t.text "exam_url"
+    t.text "answer_text"
     t.index ["user_id"], name: "index_answers_on_user_id"
   end
 
