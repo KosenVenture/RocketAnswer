6cd9258f9c29355ed3a56495f623fef30841639c
Hayato OKUMOTO <falcon@falconsrv.net>
2017-09-03 16:29:10 +0900

    体験談投稿でも一覧に現れるようにクエリを修正

--- app/models/university.rb
+++ app/models/university.rb
@@ -3,6 +3,11 @@ class University < School
     through: :departments
   has_many :exp_stories,
   	through: :departments
-  scope :answer_exists,
-    -> { joins(:answers).uniq }
+
+    scope :answer_exists, -> {
+        where(id: Answer.joins(:department).select('departments.school_id').reorder(''))
+        .or(
+          where(id: ExpStory.joins(:department).select('departments.school_id').reorder(''))
+        )
+      }
 end
