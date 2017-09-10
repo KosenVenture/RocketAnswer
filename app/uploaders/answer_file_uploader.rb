5d89890b4a7cbf78fcb2d88a64096cd8419dce2f
Hayato OKUMOTO <falcon@falconsrv.net>
2017-09-10 16:52:04 +0900

    Fix typo

--- app/uploaders/answer_file_uploader.rb
+++ app/uploaders/answer_file_uploader.rb
@@ -10,7 +10,7 @@ class AnswerFileUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  def extension_white_list
+  def extension_whitelist
     %w(jpg jpeg png pdf)
   end
 end
