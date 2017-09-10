bc79f137498d024e9b1c6847da202c6e81fab530
Hayato OKUMOTO <falcon@falconsrv.net>
2017-09-10 23:11:49 +0900

    productionのメール設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = Settings.smtp.to_h
+  config.action_mailer.default_url_options = { host: Settings.mail.default_host, protocol: 'https' }
 end
