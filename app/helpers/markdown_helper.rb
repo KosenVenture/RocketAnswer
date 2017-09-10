d10d892d6cd6aab8d6dba5ca6ebcfb4c1ef65632
Hayato OKUMOTO <falcon@falconsrv.net>
2017-09-10 16:50:47 +0900

    Markdownのサニタイズ

--- app/helpers/markdown_helper.rb
+++ app/helpers/markdown_helper.rb
@@ -1,12 +1,16 @@
 module MarkdownHelper
   def markdown(text)
     unless false#@markdown
-      renderer = Redcarpet::Render::HTML.new(filter_html: true, hard_wrap: true)
+      renderer = Redcarpet::Render::HTML.new(
+        filter_html: true,
+        hard_wrap: true,
+        link_attributes: { target: '_blank' }
+      )
       @markdown = Redcarpet::Markdown.new(renderer,
-        autolink: true,
-        no_intra_emphasis: true)
+        no_intra_emphasis: true,
+      )
     end
 
-    @markdown.render(text).html_safe
+    sanitize @markdown.render(text)
   end
 end
